9d8f97d3c2d7f6de367276aa0d5c5bdf571b5cd2
Jonas Curth <mail@jonascurth.de>
2026-03-08 14:20:18 +0100

    feat: add roles table migration (#83)

---
+++ migrations/022_custom_roles.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE project_members DROP CONSTRAINT IF EXISTS project_members_role_fkey;
+
+ALTER TABLE project_members ADD CONSTRAINT project_members_role_check CHECK (role IN ('admin', 'editor', 'viewer'));
+
+DROP TABLE IF EXISTS roles;
